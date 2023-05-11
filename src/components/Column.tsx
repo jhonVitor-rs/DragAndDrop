259983e5aac79473410ebc319c9138187a387586
jhonVitor-rs <68336988+jhonVitor-rs@users.noreply.github.com>
2023-05-11 20:03:23 -0300

    Update Column.tsx

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,4 +1,5 @@
 import columnDrop from "../hooks/columnDrop"
+import { ColumnType } from "../types/Enum"
 import { IColumn } from "../types/Models"
 import Task from "./Tasks"
 
@@ -10,9 +11,11 @@ interface ColumnProps{
 function Column({column, index: columnIndex}: ColumnProps) {
   const {ref} = columnDrop<HTMLDivElement>({column, columnIndex})
 
+  const opacity = column.title === ColumnType.COMPLETED ? 'opacity-50' : ''
+
   return (
     <div 
-      className={`bg-gray-200 p-4 max-w-2xl w-full m-3 text-center items-center rounded-lg flex flex-col gap-4`} 
+      className={`bg-gray-200 ${opacity} p-4 max-w-2xl w-full m-3 text-center items-center rounded-lg flex flex-col gap-4`} 
       ref={ref}
     >
       <h2 className="text-lg font-bold mb-4 bg-gray-400 w-full text-center rounded-md">{column.title}</h2>
